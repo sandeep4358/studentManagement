f6e16c43af4888dbe8320e72ae924d05b81998ac
sandeep4358 <sandeep4358@gmail.com>
2023-11-28 23:04:55 +0530

    new changes

--- src/main/java/com/javafruit/StudentManagment/StudentManagmentApplication.java
+++ src/main/java/com/javafruit/StudentManagment/StudentManagmentApplication.java
@@ -2,14 +2,20 @@ package com.javafruit.StudentManagment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
-public class StudentManagmentApplication {
+public class StudentManagmentApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagmentApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(StudentManagmentApplication.class);
+	}
 }
